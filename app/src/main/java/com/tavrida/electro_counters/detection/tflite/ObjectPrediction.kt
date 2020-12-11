b493ef42cf8f257507a2248ddcb89f4b93a0ac4b
Vladimir <iam.vladimir.tretyak@gmail.com>
2020-12-11 14:19:08 +0300

    delete tflite stuff

--- app/src/main/java/com/tavrida/electro_counters/detection/tflite/ObjectPrediction.kt
+++
@@ -1,5 +0,0 @@
-package com.tavrida.electro_counters.detection.tflite
-
-import android.graphics.RectF
-
-data class ObjectPrediction(val location: RectF, val labelId: Int, val label: String, val score: Float)
