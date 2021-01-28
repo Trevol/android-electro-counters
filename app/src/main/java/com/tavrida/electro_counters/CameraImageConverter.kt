beb5ddb3e9c2e2ceac5377f3f78072943f52a85f
Vladimir <iam.vladimir.tretyak@gmail.com>
2021-01-28 09:22:42 +0300

    separate class for image conversions

--- app/src/main/java/com/tavrida/electro_counters/CameraImageConverter.kt
+++ app/src/main/java/com/tavrida/electro_counters/CameraImageConverter.kt
@@ -5,24 +5,31 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import androidx.camera.core.ImageProxy
 import com.tavrida.utils.camera.YuvToRgbConverter2
+import com.tavrida.utils.compensateSensorRotation
+import java.lang.IllegalStateException
 
 class CameraImageConverter(context: Context) {
-    private val forProcessing = BitmapProxy()
-    private var forDisplay = BitmapProxy()
+    private val conversionBuffer = BitmapProxy()
+    private var displayBuffer = BitmapProxy()
     private val yuvToRgbConverter = YuvToRgbConverter2(context)
 
-    data class ConvertResult(val processing: Bitmap, val display: Bitmap)
+    data class ConvertResult(val readyForProcessing: Bitmap, val readyForDisplay: Bitmap)
+
+    private fun ImageProxy.fromYuvToRgb(): Bitmap {
+        val converted = conversionBuffer.bitmapLike(this)
+        yuvToRgbConverter.yuvToRgb(
+            this,
+            converted
+        )
+        return converted
+    }
 
     fun convert(cameraImage: ImageProxy): ConvertResult {
-        // cameraImage => processingBitmap
-        val processing = forProcessing.bitmapLike(cameraImage)
-        yuvToRgbConverter.yuvToRgb(
-            cameraImage,
-            processing
-        )
-        // processingBitmap => displayBitmap
-        val display = forProcessing.copyTo(forDisplay)
-        return ConvertResult(processing, display)
+        val readyForProcessing = cameraImage.fromYuvToRgb()
+            .compensateSensorRotation(cameraImage.imageInfo.rotationDegrees)
+        displayBuffer.copyFrom(readyForProcessing)
+
+        return ConvertResult(readyForProcessing, displayBuffer.bitmap())
     }
 }
 
@@ -33,19 +40,25 @@ private class BitmapProxy {
     private fun bitmap(width: Int, height: Int): Bitmap {
         if (bitmap == null) {
             bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        } else {
+            if (bitmap().width != width || bitmap().height != height) {
+                throw IllegalStateException("bitmap().width != width || bitmap().height != height")
+            }
         }
         return bitmap!!
     }
 
+    fun bitmap() = bitmap!!
 
     fun bitmapLike(image: ImageProxy) = bitmap(image.width, image.height)
+    fun bitmapLike(image: Bitmap) = bitmap(image.width, image.height)
 
-    fun copyTo(dst: BitmapProxy): Bitmap {
-        val src = bitmap!!
+    fun copyFrom(src: Bitmap): BitmapProxy {
+        val dst = bitmapLike(src)
         if (canvas == null) {
-            canvas = Canvas(dst.bitmap(src.width, src.height))
+            canvas = Canvas(dst)
         }
         canvas!!.drawBitmap(src, 0f, 0f, null)
-        return bitmap!!
+        return this
     }
 }
