11ce2c4496a713955bd5b5b043b51fdea668cb28
Vladimir <iam.vladimir.tretyak@gmail.com>
2021-02-09 19:17:38 +0300

    frames recorder

---
+++ app/src/main/java/com/tavrida/electro_counters/FramesRecorder.kt
@@ -0,0 +1,102 @@
+package com.tavrida.electro_counters
+
+import android.graphics.Bitmap
+import android.os.Environment
+import com.tavrida.utils.assert
+import com.tavrida.utils.saveAsJpeg
+import com.tavrida.utils.zeroPad
+import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FramesRecorder(storageDir: File, subDir: String = "frames") {
+    private val framesDir = File(storageDir, subDir)
+
+    private var framesPos = 0
+    private var sessionId = ""
+    private var started = false
+
+    fun toggleSession(started: Boolean) =
+        if (started) {
+            startSession()
+        } else {
+            stopSession()
+        }
+
+
+    private fun startSession() {
+        (!started).assert()
+        framesDir.mkdirs()
+        sessionId = createTimestamp()
+        started = true
+    }
+
+    private fun stopSession() {
+        started.assert()
+        framesPos = 0
+        sessionId = ""
+        started = false
+    }
+
+    fun addFrame(frame: Bitmap) {
+        started.assert()
+
+        framesPos.zeroPad(5)
+            .let { paddedPos ->
+                "${sessionId}_${paddedPos}.jpg"
+            }
+            .let { fn ->
+                File(framesDir, fn)
+            }.also { f ->
+                frame.saveAsJpeg(f, JPEG_QUALITY)
+            }
+
+        framesPos++
+    }
+
+    private companion object {
+        private const val JPEG_QUALITY = 75
+        private const val TIMESTAMP_FORMAT = "yyyyMMddHHmmss"
+        private fun createTimestamp() =
+            SimpleDateFormat(TIMESTAMP_FORMAT, Locale.US).format(System.currentTimeMillis())
+    }
+
+}
+
+fun prepareFramesRecorder(
+    externalStorageDir: String,
+    internalStorageDir: File,
+    subDir: String = "frames"
+): FramesRecorder {
+    //try to external storage
+    val (storageDir, _) = createExternalStorageDir(externalStorageDir)
+    if (storageDir != null) {
+        return FramesRecorder(storageDir, subDir)
+    }
+    // fallback to internal files storage
+    return FramesRecorder(internalStorageDir, subDir)
+}
+
+private data class CreateExternalStorageDirResult(val externalDir: File?, val errorReason: String?)
+
+private fun createExternalStorageDir(dirName: String): CreateExternalStorageDirResult {
+    try {
+        if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED) {
+            return CreateExternalStorageDirResult(
+                null,
+                "ExternalStorage unavailable: ${Environment.getExternalStorageState()}"
+            )
+        }
+        val extDir = File(Environment.getExternalStorageDirectory(), dirName)
+        extDir.mkdir()
+        File(extDir, "test.txt").apply {
+            //TODO: check access by creating and deleting file
+            createNewFile()
+            delete()
+        }
+        return CreateExternalStorageDirResult(extDir, "")
+    } catch (e: Exception) {
+        return CreateExternalStorageDirResult(null, e.message)
+    }
+}
