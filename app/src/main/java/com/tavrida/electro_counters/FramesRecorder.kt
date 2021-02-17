59b54926ec9a23358d2424c6299383be6b1993d5
Vladimir <iam.vladimir.tretyak@gmail.com>
2021-02-17 13:41:55 +0300

    record to session dirs

--- app/src/main/java/com/tavrida/electro_counters/FramesRecorder.kt
+++ app/src/main/java/com/tavrida/electro_counters/FramesRecorder.kt
@@ -2,53 +2,43 @@ package com.tavrida.electro_counters
 
 import android.graphics.Bitmap
 import com.tavrida.utils.Timestamp
-import com.tavrida.utils.assert
 import com.tavrida.utils.saveAsJpeg
 import com.tavrida.utils.zeroPad
 import java.io.File
 
 class FramesRecorder(storage: AppStorage, subDir: String = "frames", var enabled: Boolean) {
     private val framesDir = File(storage.root, subDir)
-    private var sessionDir: File? = null
+    private var sessionDir = File("", "")
+    private var sessionDirCreated = false
 
     private var framesPos = 0
     private var sessionId = ""
-    private var started = false
 
-    fun toggleSession(started: Boolean) =
+    fun toggleSession(started: Boolean) {
         if (started) {
-            startSession()
-        } else {
-            stopSession()
+            newSession()
         }
-
-
-    private fun startSession() {
-        (!started).assert()
-        sessionId = Timestamp.current()
-        framesDir.mkdirs()
-        started = true
     }
 
-    private fun stopSession() {
-        started.assert()
+    private fun newSession() {
         framesPos = 0
-        sessionId = ""
-        started = false
+        sessionId = Timestamp.current()
+        sessionDirCreated = false
+        sessionDir = File(framesDir, sessionId)
     }
 
     fun record(frame: Bitmap) {
         if (!enabled) {
             return
         }
-        started.assert()
-
-        framesPos.zeroPad(5)
+        checkMaxFrames()
+        createSessionDirIfNeeded()
+        framesPos.zeroPad(FRAME_POS_LEN)
             .let { paddedPos ->
                 "${sessionId}_${paddedPos}.jpg"
             }
             .let { fn ->
-                File(framesDir, fn)
+                File(sessionDir, fn)
             }.also { f ->
                 frame.saveAsJpeg(f, JPEG_QUALITY)
             }
@@ -56,7 +46,23 @@ class FramesRecorder(storage: AppStorage, subDir: String = "frames", var enabled
         framesPos++
     }
 
+    private fun createSessionDirIfNeeded() {
+        if (sessionDirCreated) {
+            return
+        }
+        sessionDir!!.mkdirs()
+    }
+
+    private fun checkMaxFrames() {
+        if (framesPos > 99999) {
+            throw IllegalStateException("framesPos > 99999")
+        }
+    }
+
     private companion object {
+        const val MAX_FRAMES_PER_SESSION = 99999
+        const val FRAME_POS_LEN = MAX_FRAMES_PER_SESSION.toString().length
+
         private const val JPEG_QUALITY = 75
     }
 
