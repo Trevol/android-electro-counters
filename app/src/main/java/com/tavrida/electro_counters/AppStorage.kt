8f6943240058608630594d0ea4973e450ac91e6b
Vladimir <iam.vladimir.tretyak@gmail.com>
2021-02-17 08:43:41 +0300

    refactoring in AppStorage

--- app/src/main/java/com/tavrida/electro_counters/AppStorage.kt
+++ app/src/main/java/com/tavrida/electro_counters/AppStorage.kt
@@ -5,38 +5,42 @@ import android.os.Environment
 import java.io.File
 
 class AppStorage(context: Context, storageDir: String) {
-    val root: File
-
-    init {
-        root = getRootDir(context, storageDir)
-    }
+    val root: File = getRootDir(context, storageDir)
 
     private fun getRootDir(context: Context, storageDir: String): File {
         val (extDir) = tryExternalStorage(storageDir)
         return extDir?.let { it } ?: context.filesDir
     }
 
-    private data class ValueOrError<T>(val value: T?, val error: String?)
 
     private fun tryExternalStorage(dirName: String): ValueOrError<File> {
         try {
             if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED) {
-                return ValueOrError(
-                    null,
+                return ValueOrError.error(
                     "ExternalStorage unavailable: ${Environment.getExternalStorageState()}"
                 )
             }
             val extDir = File(Environment.getExternalStorageDirectory(), dirName)
             extDir.mkdir()
-            File(extDir, "test.txt").apply {
-                //TODO: check access by creating and deleting file
-                createNewFile()
-                delete()
-            }
-            return ValueOrError(extDir, "")
+            checkRwAccess(extDir)
+            return ValueOrError.value(extDir)
         } catch (e: Exception) {
-            return ValueOrError(null, e.message)
+            return ValueOrError.error(e.message)
         }
+    }
+
+    private fun checkRwAccess(extDir: File) {
+        File(extDir, "test.txt").apply {
+            createNewFile()
+            delete()
+        }
+    }
+}
+
+private data class ValueOrError<T>(val value: T?, val error: String?) {
+    companion object Default {
+        fun <T> error(err: String?) = ValueOrError(null as T?, err)
+        fun <T> value(value: T) = ValueOrError(value, null)
     }
 }
 
