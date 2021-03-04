c76d4b609f9ab38b80a470ddbcf9d4ce4e28a265
Vladimir <iam.vladimir.tretyak@gmail.com>
2021-03-04 16:43:54 +0300

    remove commented

--- counter_scanner/src/main/java/com/tavrida/counter_scanner/aggregation/AggregatingBoxGroupingDigitExtractor.kt
+++ counter_scanner/src/main/java/com/tavrida/counter_scanner/aggregation/AggregatingBoxGroupingDigitExtractor.kt
@@ -21,27 +21,6 @@ class AggregatingBoxGroupingDigitExtractor {
             }
     }
 
-    /*fun aggregateDetections(
-        currentDetections: Collection<DigitDetectionResult>,
-        prevDetections: Collection<AggregatedDetections>
-    ): List<AggregatedDetections> {
-        val boxes = currentDetections.map { it.location } +
-                prevDetections.map { it.location }
-        val scores = currentDetections.map { it.score } + prevDetections.map { it.score }
-        val currentFirstScores =
-            currentDetections.map { it.score + 1 } + prevDetections.map { it.score }
-        val digitsCounts = currentDetections.map { listOf(DigitCount(it.digit, 1)) } +
-                prevDetections.map { it.digitsCounts }
-        //TODO: boxes from detections have priority - should be in keptIndices
-        //create synthetic score by adding +1 to detected boxes and keep score unchanged for tracked boxes
-        return groupBoxes(boxes, currentFirstScores, .04f)
-            .groupIndices.zip(digitsCounts)
-            .groupBy({ it.first }, { it.second })
-            .map { index, digitsCountsByBox ->
-                AggregatedDetections(boxes[index], scores[index], merge(digitsCountsByBox))
-            }
-    }*/
-
     fun extractDigits(detections: Collection<AggregatedDetections>) = detections
         .filter { it.totalCount >= minBoxesInGroup }
         .map { DigitAtLocation(it.digitWithMaxCount, it.location) }
