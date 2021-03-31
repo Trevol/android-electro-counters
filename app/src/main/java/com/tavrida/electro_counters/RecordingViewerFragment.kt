276fda27a4d61d0dc8c8f18135323b881ca8a6b6
Vladimir <iam.vladimir.tretyak@gmail.com>
2021-03-31 23:01:35 +0300

    navigation

--- app/src/main/java/com/tavrida/electro_counters/RecordingViewerFragment.kt
+++ app/src/main/java/com/tavrida/electro_counters/RecordingViewerFragment.kt
@@ -1,5 +1,6 @@
 package com.tavrida.electro_counters
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -21,15 +22,18 @@ class RecordingViewerFragment(val sessionDir: File) : DialogFragment() {
     lateinit var forwardFastBtn: Button
     lateinit var closeBtn: Button
 
-    private val items = ImagesNavigation(imageFiles(sessionDir)).apply { setToMiddle() }
+    private val items =
+        imageFiles(sessionDir).let { files -> ImagesNavigation(files, files.size / 2) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.recording_viewer, container, false)
+    ) = inflater.inflate(R.layout.recording_viewer, container, false)
+        .also { bindUI(it) }
+        .also { setupUI() }
 
+    private fun bindUI(view: View) {
         imgView = view.findViewById(R.id.recorded_image_view)
         positionTxt = view.findViewById(R.id.position_txt)
         backBtn = view.findViewById(R.id.back_btn)
@@ -38,21 +42,37 @@ class RecordingViewerFragment(val sessionDir: File) : DialogFragment() {
         forwardFastBtn = view.findViewById(R.id.forwardFast_btn)
 
         closeBtn = view.findViewById(R.id.close_btn)
+    }
+
+    private fun setupUI() {
         closeBtn.setOnClickListener { close() }
-
-        setupGallery()
-        return view
-    }
-
-    private fun setupGallery() {
-        TODO()
-    }
-
-    private fun loadFirstImage() = glob(File(sessionDir, "*.jpg"))
-        .sorted().first()
-        .let {
-            BitmapFactory.decodeFile(it.absolutePath)
+        backBtn.setOnClickListener {
+            if (items.move(-1)) {
+                updateImageAndPosition()
+            }
         }
+        backFastBtn.setOnClickListener {
+            if (items.move(-FAST_ITEMS)) {
+                updateImageAndPosition()
+            }
+        }
+        forwardBtn.setOnClickListener {
+            if (items.move(1)) {
+                updateImageAndPosition()
+            }
+        }
+        forwardFastBtn.setOnClickListener {
+            if (items.move(FAST_ITEMS)) {
+                updateImageAndPosition()
+            }
+        }
+        updateImageAndPosition()
+    }
+
+    private fun updateImageAndPosition() {
+        imgView.setImageBitmap(items.currentImage)
+        positionTxt.text = items.positionDesc
+    }
 
     private fun close() {
         if (dialog != null) {
@@ -65,12 +85,41 @@ class RecordingViewerFragment(val sessionDir: File) : DialogFragment() {
     }
 
     private companion object {
+        const val FAST_ITEMS = 30
         fun imageFiles(sessionDir: File) = glob(File(sessionDir, "*.jpg")).sorted()
     }
 }
 
-private class ImagesNavigation(imageFiles: List<File>) {
-    fun setToMiddle() {
-        TODO("Not yet implemented")
+private class ImagesNavigation(val imageFiles: List<File>, navigateTo: Int = 0) {
+    var currentImage: Bitmap? = null
+        private set
+    private var position = 0
+    val positionDesc get() = "${position + 1}/${imageFiles.size}"
+
+    init {
+        move(navigateTo)
     }
+
+    fun move(numOfItems: Int): Boolean {
+        val newPos = clip(position + numOfItems, 0, imageFiles.size - 1)
+        val stateChanged = newPos != position
+        position = newPos
+        if (stateChanged || currentImage == null) {
+            currentImage = decode(imageFiles[position])
+        }
+        return stateChanged
+    }
+
+    private companion object {
+        fun decode(path: File) = BitmapFactory.decodeFile(path.absolutePath)
+        fun clip(value: Int, lower: Int, upper: Int): Int {
+            if (value < lower)
+                return lower
+            if (value > upper)
+                return upper
+            return value
+        }
+    }
+
+
 }
